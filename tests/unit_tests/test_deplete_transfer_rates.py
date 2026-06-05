52cace27e77e29067c5cf551e65edabc78807c35
imronuke <makrus.imron@gmail.com>
2026-06-05 09:35:33 +0400

    tests added

--- tests/unit_tests/test_deplete_transfer_rates.py
+++ tests/unit_tests/test_deplete_transfer_rates.py
@@ -199,6 +199,16 @@ def test_transfer(run_in_tmpdir, model):
     assert atoms[1] == pytest.approx(atoms[0]*exp(-transfer_rate*3600*24))
     assert atoms[2] == pytest.approx(atoms[1]*exp(-transfer_rate*3600*24))
 
+
+def test_tt_depletion_disallows_transfer_rates(model):
+    op = CoupledOperator(model, CHAIN_PATH, tt_eps=1e-3)
+    integrator = openmc.deplete.PredictorIntegrator(
+        op, [1], 0.0, timestep_units='d')
+
+    with pytest.raises(ValueError, match='Transfer rates are not supported'):
+        integrator.add_transfer_rate('f', ['U'], 1e-5)
+
+
 @pytest.mark.parametrize("case_name, buffer, ox", [
     ('redox', {'Gd157':1}, {'Gd': 3, 'U': 4}),
     ('buffer_invalid', {'Gd158':1}, {'Gd': 3, 'U': 4}),
